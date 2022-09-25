a4ad1caa83b62091f5c39c83d2a9234ef0fe88a3
Nikhil Manu <nikhilsmanu@gmail.com>
2022-09-25 17:01:32 +0530

    configured mongo

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './model/user.model';
 
 @Module({
-  providers: [UsersService, UsersResolver]
+  providers: [UsersService, UsersResolver],
+  exports: [UsersService],
+  imports: [TypeOrmModule.forFeature([User])]
 })
 export class UsersModule {}
